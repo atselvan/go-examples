3f04bb7459aa652516bfc418e0c39cb930052ecd
Allan Tony Selvan <allan.tony.selvan@nl.abnamro.com>
2019-01-05 20:59:05 +0100

    Initial Commit

---
+++ webdriver.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"time"
+	"github.com/fedesog/webdriver"
+)
+
+func main() {
+	chromeDriver := webdriver.NewChromeDriver("/usr/local/bin/chromedriver")
+	err := chromeDriver.Start()
+	if err != nil {
+		log.Println(err)
+	}
+	desired := webdriver.Capabilities{"Platform": "Linux"}
+	required := webdriver.Capabilities{}
+	session, err := chromeDriver.NewSession(desired, required)
+	if err != nil {
+		log.Println(err)
+	}
+	err = session.Url("http://golang.org")
+	if err != nil {
+		log.Println(err)
+	}
+	time.Sleep(60 * time.Second)
+	session.Delete()
+	chromeDriver.Stop()
+}
